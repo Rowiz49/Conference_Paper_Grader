f41107ac7239985607f4065559d89fd315544dc8
Rowiz49 <adrianpuyetm@gmail.com>
2025-11-04 20:14:57 +0100

    Improve new conference form

--- app/paper_grader/forms.py
+++ app/paper_grader/forms.py
@@ -7,12 +7,27 @@ class ConferenceForm(forms.ModelForm):
     class Meta:
         model = Conference
         fields = ["name"]
+        widgets = {
+            "name": forms.TextInput(
+                attrs={"placeholder": "Conference name"}
+            ),
+        }
 
 
 QuestionFormSet = inlineformset_factory(
     Conference,
     Question,
     fields=["question_text"],
+    exclude=["id"], 
+    widgets={
+        "question_text": forms.Textarea(
+            attrs={
+                "class": "textarea textarea-sm w-full",
+                "rows": 1,
+                "placeholder": "Enter a question...",
+            }
+        )
+    },
     extra=1,
     can_delete=True
 )
